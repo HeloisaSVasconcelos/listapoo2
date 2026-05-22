3fc2fbde157c244ca1b305ef7f15a49e1ff42514
souzaheloisa277@gmail.com <HeloisaSVasconcelos>
2026-05-21 20:17:01 -0400

    exercicio3

--- src/wrappers/Exercicio3.java
+++ src/wrappers/Exercicio3.java
@@ -1,17 +1,17 @@
-package principal;
+package wrappers;
 
-public class Main {
+public class Exercicio3 {
     public static void main(String[] args) {
         Integer a = 127;
         Integer b = 127;
-        // true devido ao cache do Java para valores de -128 a 127
+        // Retorna true devido ao cache do Java para valores de -128 a 127
         System.out.println(a == b);
 
         Integer c = 200;
         Integer d = 200;
-        // false porque sao objetos diferentes na memoria
+        // Retorna false porque são objetos diferentes na memória
         System.out.println(c == d);
-        // true pois compara o valor interno dos objetos
+        // Retorna true pois compara o valor interno dos objetos
         System.out.println(c.equals(d));
     }
 }
