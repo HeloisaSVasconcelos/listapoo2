fb6acd835fc9defcfcfb41ca2c699cde889586c0
souzaheloisa277@gmail.com <HeloisaSVasconcelos>
2026-05-21 20:18:08 -0400

    exercicio4

--- src/wrappers/Exercicio4.java
+++ src/wrappers/Exercicio4.java
@@ -1,8 +1,8 @@
-package principal;
+package wrappers;
 
 import java.math.BigDecimal;
 
-public class Main {
+public class Exercicio4 {
     public static void main(String[] args) {
         BigDecimal preco = new BigDecimal("49.90");
         BigDecimal qtd = new BigDecimal("3");
@@ -10,6 +10,7 @@ public class Main {
 
         BigDecimal total = preco.multiply(qtd).multiply(desconto);
 
+        // Imprime o total com o desconto aplicado
         System.out.println("Total: R$ " + total);
     }
 }
